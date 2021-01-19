1101661066e66852f35f8b70ea18a284aeebf04c
Julia <69167984+Julia-Kalyukh@users.noreply.github.com>
2021-01-20 01:44:06 +0300

    commit_lesson2

--- js/main.js
+++ js/main.js
@@ -1,40 +1,72 @@
+class Product {
+    constructor(product, img = 'https://placehold.it/120x100') {
+        let { title = "Название товара", price = 100, id } = product;
+        this.title = title;
+        this.img = img;
+        this.price = price;
+        this.id = id;
+    }
+
+    render() {
+        return `<div class="product_item">
+                  <img src="${this.img}" alt="${this.title}">
+                  <div class="description">
+                      <h3 class="title">${this.title}</h3>
+                      <p class="price">${this.price}</p>
+                      <button class="buy_btn">Добавить</button>
+                  </div>
+              </div>`
+    }
+
+}
+
+class ProductsList {
+    constructor(container = '.products') {
+        this.data = [];
+        this.products = [];
+        this.container = document.querySelector(container);
+        this._fetchData();
+        this._render();
+    }
+
+    init() {}
+
+    _fetchData() {
+        this.data = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Keyboard', price: 200 },
+            { id: 3, title: 'Mouse', price: 100 },
+            { id: 4, title: 'Gamepad', price: 87 },
+            { id: 5, title: 'Notebook', price: 2500},
+            { id: 6, title: 'Keyboard', price: 150 },
+            { id: 7, title: 'Mouse', price: 300 },
+            { id: 8, title: 'Gamepad', price: 500},
+            { id: 9, title: 'Notebook', price: 3000 },
+            { id: 10, title: 'Keyboard', price: 100 },
+            { id: 11, title: 'Mouse', price: 190 },
+            { id: 12, title: 'Gamepad', price: 56 },
+            { id: 13, title: 'Notebook', price: 4000},
+            { id: 14, title: 'Keyboard', price: 1000 },
+            { id: 15, title: 'Mouse', price: 700 },
+            { id: 16, title: 'Gamepad', price: 1100},
+        ];
+    }
+
+    _render() {
+        for (let dataEl of this.data) {
+            const product = new Product(dataEl);
+            this.products.push(product);
+            this.container.insertAdjacentHTML('beforeend', product.render())
+        }
+    }
+}
+
+const list = new ProductsList();
 
 
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Keyboard', price: 200 },
-    { id: 3, title: 'Mouse', price: 100 },
-    { id: 4, title: 'Gamepad', price: 87 },
-    { id: 5, title: 'Notebook', price: 2500},
-    { id: 6, title: 'Keyboard', price: 150 },
-    { id: 7, title: 'Mouse', price: 300 },
-    { id: 8, title: 'Gamepad', price: 500},
-    { id: 9, title: 'Notebook', price: 3000 },
-    { id: 10, title: 'Keyboard', price: 100 },
-    { id: 11, title: 'Mouse', price: 190 },
-    { id: 12, title: 'Gamepad', price: 56 },
-    { id: 13, title: 'Notebook', price: 4000},
-    { id: 14, title: 'Keyboard', price: 1000 },
-    { id: 15, title: 'Mouse', price: 700 },
-    { id: 16, title: 'Gamepad', price: 1100},
-];
+//class Cart {
+    //constructor() {
+        // name - свойство в котором лежит имя
+    
 
-
-const renderProduct = (product, img = 'https://placehold.it/120x100') => {
-    const{title = "Название товара", price = 100} = product;
-    return `<div class="product_item">
-    <img src="${img}" alt="${title}">
-    <div class="description">
-    <h3 class="title">${title}</h3>
-    <p class="price">${price}</p>
-    <button class="buy_btn">Добавить</button>
-    </div>
-    </div>`
-};
-
-const render = productsList => {
-    const productsElements = productsList.map(product => renderProduct(product)).join('');
-    document.querySelector('.products').innerHTML = productsElements;
-};
-
-render(products);
+    // some() - метод для подсчета стоимости корзины
